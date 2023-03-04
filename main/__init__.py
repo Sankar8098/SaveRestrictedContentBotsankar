3fd990f91e785132ce6eeca9dad9af92399fb137
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-03-04 12:36:15 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = API_ID
-API_HASH = 
-BOT_TOKEN = 
-SESSION = 
-FORCESUB = 
-AUTH = 
+API_ID = 29409646
+API_HASH = a69d0340a520c1913c517bea143a3de7
+BOT_TOKEN = 5870879185:AAGZrye09-kxmJsmSa-rSqKT38FPoyfbgW8
+SESSION = BQB8a3KQ1HsM1YfNU-1AIk9meM2P1ZQo_nCK3Mk1xmbiy3mtnNNuGOE7i77AGOBjhpkfr5aCAUYsUXCDOotd9eXHW-bDfSWF_cZRFS7YDCuzExHLnxMIb8aLjJMdnTqQS7iXynb-ciNP0I5_PDBzvWDilBzH857Xz4LeT1Gdt2hya671KPq4RrEy01dUsGpEWVNw6oC8_R_juseA1sS0Qump9KaPHHdgqtd0wC8iTFD4Lx6nFA8WG61CSUCcJXWSSf4ql9gipDetAxNKo0t8VPzYokNszt-13Ulkin3C-GjP6Orqu2c7qvkL0ronvBlIITRJKWkItEkKnhZTxLTtwLjTAAAAAUuyxcIA
+FORCESUB = SK_MoviesOffl
+AUTH = 5564974530
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
